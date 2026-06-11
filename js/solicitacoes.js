9fb0e2b77264bbe175878404a8507e5b056a2c70
Mouzer <mouzerskt8@gmail.com>
2026-06-11 10:51:34 -0300

    Update solicitacoes.js

--- js/solicitacoes.js
+++ js/solicitacoes.js
@@ -227,7 +227,7 @@ async function processarOS(id, uid, tipo, nome) {
                 solicitacao_id: idCustom,
                 usuario_id: uid,
                 usuario_nome: nome,
-                    matricula: currentUser.matricula,
+                    matricula: os.matricula,
                 equipamento_cod: patrimonio,
                 tipo: tipo,
                 descricao: descricaoOriginalDaOS,
